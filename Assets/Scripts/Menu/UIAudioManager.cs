6f5a24d64a61574f4e5573449337bf7d79e72929
1103-Velasquez-Ernest <143988150+1103-Velasquez-Ernest@users.noreply.github.com>
2025-11-08 01:33:13 -0800

    Added menu animation + sfx
    Added menu animation with buttons appearing one by one in addition to early click sfx

---
+++ Assets/Scripts/Menu/UIAudioManager.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class UIAudioManager : MonoBehaviour
+{
+    public static UIAudioManager Instance { get; private set; }
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public float defaultVolume = 0.7f;
+    void Awake()
+    {
+        
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        if(audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();   
+            audioSource.playOnAwake = false;
+            audioSource.loop = false;
+        }
+        audioSource.volume = defaultVolume;
+    }
+    
+    public void PlayOneShot(AudioClip clip, float volume = 1f)
+    {
+        if (clip == null || audioSource == null) return;
+        audioSource.PlayOneShot(clip, volume);
+    }
+}
