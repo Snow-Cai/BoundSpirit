6ee788670a51ff824f5344b94bbb3776bdef4dc5
1103-Velasquez-Ernest <143988150+1103-Velasquez-Ernest@users.noreply.github.com>
2025-11-12 16:45:54 -0800

    Adjusted staircase sprite and fixed moving downstairs visual

--- Assets/Scripts/House/StairsTrigger.cs
+++ Assets/Scripts/House/StairsTrigger.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 public class StairsTrigger : MonoBehaviour
 {
     public FloorTransition floorTransitiion;
+    public bool goingUp = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if (!other.CompareTag("Player")) return;
+
+        SpriteRenderer playerRenderer = other.GetComponent<SpriteRenderer>();
+        if(playerRenderer == null) return;
+
+        if(goingUp)     //player goes up and appears above the stairs
         {
-            floorTransitiion.TriggerTransition();
+            playerRenderer.sortingLayerName = "Player";
+            goingUp = false;
         }
+        else            //player goes down and appears above stairs but below second floor 
+        {
+            playerRenderer.sortingLayerName = "BelowSecondFloor";
+            goingUp= true;
+        }
+            floorTransitiion.TriggerTransition();
     }
 }
